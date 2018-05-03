9af8abd9c84cc639fa48f237dce14124ec641b48
Rob Sanheim <rsanheim@gmail.com>
2018-05-02 22:39:38 -0500

    remove log options
    these are set in application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,6 @@ Heaven::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -75,7 +74,4 @@ Heaven::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
 end
