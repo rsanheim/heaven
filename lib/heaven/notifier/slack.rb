faf8f795a3428a0c4bd73dc4bbca7f2c08e39c9a
colemanja91 <colemanja91@gmail.com>
2018-11-15 19:46:46 -0500

    add turnkey notifier

--- lib/heaven/notifier/slack.rb
+++ lib/heaven/notifier/slack.rb
@@ -31,7 +31,12 @@ module Heaven
         message << " : #{user_link}"
         case state
         when "success"
-          message << "'s #{environment} deployment of #{repository_link} is done! "
+          if environment == "turnkey"
+            turnkey_url = "https://brainy-tk#{custom_payload['pull_request_id']}.first.io"
+            message << "'s #{environment} deployment of #{repository_link} is done! Available at #{turnkey_url} "
+          else
+            message << "'s #{environment} deployment of #{repository_link} is done! "
+          end
         when "failure"
           message << "'s #{environment} deployment of #{repository_link} failed. "
         when "error"
