03f5f28ad7c06fb8c117ce7b92f451e5c48474bc
colemanja91 <colemanja91@gmail.com>
2018-11-27 14:24:45 -0500

    put this somewhere it actually works

--- app/models/concerns/deployment_timeout.rb
+++ app/models/concerns/deployment_timeout.rb
@@ -3,7 +3,7 @@ module DeploymentTimeout
   extend ActiveSupport::Concern
 
   def timeout
-    Integer(ENV["DEPLOYMENT_TIMEOUT"] || "300")
+    Integer(ENV["DEPLOYMENT_TIMEOUT"] || "600")
   end
 
   def deployment_time_elapsed
